7bd80d1958740cb1cb25889eb2e664ff69522c2b
alin724 <alin.tudor7@gmail.com>
2021-05-13 16:35:53 +0100

    tb: Fix regmap testbenches from jesd204 and dmac

--- library/axi_dmac/tb/regmap_tb.v
+++ library/axi_dmac/tb/regmap_tb.v
@@ -42,7 +42,6 @@ module regmap_tb;
   `include "tb_base.v"
 
   localparam DMA_LENGTH_WIDTH = 24;
-  localparam DMA_LENGTH_ALIGN = 3;
   localparam BYTES_PER_BEAT = 1;
   localparam DMA_AXI_ADDR_WIDTH = 32;
 
@@ -54,12 +53,13 @@ module regmap_tb;
 
   localparam VAL_DBG_SRC_ADDR = 32'h76543210;
   localparam VAL_DBG_DEST_ADDR = 32'hfedcba98;
-  localparam VAL_DBG_STATUS = 8'ha5;
+  localparam VAL_DBG_STATUS = 12'ha5;
   localparam VAL_DBG_IDS0 = 32'h01234567;
   localparam VAL_DBG_IDS1 = 32'h89abcdef;
 
-  localparam AW = 12;
+  localparam AW = 11;
   localparam NUM_REGS = 'h200;
+  localparam NUM_REGS1 = 'h449;
 
   wire s_axi_aclk = clk;
   wire s_axi_aresetn = ~reset;
@@ -77,7 +77,7 @@ module regmap_tb;
   wire [2:0] s_axi_arprot = 3'b000;
   wire [1:0] s_axi_rresp;
   wire [31:0] s_axi_rdata;
-
+ 
   task write_reg;
   input [31:0] addr;
   input [31:0] value;
@@ -171,13 +171,13 @@ module regmap_tb;
     for (i = 0; i < NUM_REGS; i = i + 1)
       expected_reg_mem[i] <= 'h00;
     /* Non zero power-on-reset values */
-    set_reset_reg_value('h00, 32'h00040161); /* PCORE version register */
+    set_reset_reg_value('h00, 32'h00040361); /* PCORE version register */
     set_reset_reg_value('h0c, 32'h444d4143); /* PCORE magic register */
+    set_reset_reg_value('h10, 32'h00002101); /*	INTERFACE_DESCRIPTION */
     set_reset_reg_value('h80, 'h3); /* IRQ mask */
 
-    set_reset_reg_value('h40c, 'h3); /* Flags */
+    set_reset_reg_value('h40c, 'h7); /* Flags */
     set_reset_reg_value('h418, LENGTH_ALIGN_MASK); /* Length alignment */
-
     set_reset_reg_value('h434, VAL_DBG_DEST_ADDR);
     set_reset_reg_value('h438, VAL_DBG_SRC_ADDR);
     set_reset_reg_value('h43c, VAL_DBG_STATUS);
@@ -195,7 +195,7 @@ module regmap_tb;
     end
   end
   endtask
-
+  
   task write_reg_and_update;
   input [31:0] addr;
   input [31:0] value;
@@ -232,36 +232,59 @@ module regmap_tb;
   wire [DMA_LENGTH_WIDTH-1:0] request_dest_stride;
   wire [DMA_LENGTH_WIDTH-1:0] request_src_stride;
   wire request_last;
-
+  
   reg response_eot = 1'b0;
+  wire [6:0] response_measured_burst_length = 'hff;
+  wire ctrl_enable;
+  wire ctrl_pause;
+  wire request_valid;
+  wire request_sync_transfer_start;
+  wire response_partial = 1'b1;
 
+  reg  response_valid = 1'b0;  
+  wire response_ready;
+  
+  always @(posedge clk)  begin
+    if (request_valid & request_ready) begin
+        response_valid <= 1'b1;
+    end else if (response_ready) begin
+        response_valid <= 1'b0;
+    end
+  end
+       
   integer i;
   initial begin
     initialize_expected_reg_mem();
     @(posedge s_axi_aresetn)
+    set_reset_reg_value('h44c, 32'hxxxxxxxx);
+    set_reset_reg_value('h450, 2'bX);
     check_all_registers("Initial");
-
+    //check_1("Initial");
+    
+    
     /* Check scratch */
     write_reg_and_update('h08, 32'h12345678);
     check_all_registers("Scratch");
-
+   
+    
     /* Check IRQ mask */
-    write_reg_and_update('h80, 32'h0);
     check_all_registers("IRQ mask");
-
+   
+    
     /* Check transfer registers */
-    write_reg_and_update('h40c, 'h3);
+    write_reg_and_update('h40c, 'h7);
     write_reg_and_update('h410, ADDR_MASK);
     write_reg_and_update('h414, ADDR_MASK);
     write_reg_and_update('h418, LENGTH_MASK);
     write_reg_and_update('h41c, LENGTH_MASK);
     write_reg_and_update('h420, STRIDE_MASK);
     write_reg_and_update('h424, STRIDE_MASK);
-
+    
     check_all_registers("Transfer setup 1");
-
+   
+    
     /* Check transfer registers */
-    write_reg_and_update('h40c, {$random} & 'h3);
+    write_reg_and_update('h40c, {$random} & 'h7);
     write_reg_and_update('h410, {$random} & ADDR_MASK);
     write_reg_and_update('h414, {$random} & ADDR_MASK);
     write_reg_and_update('h418, {$random} & LENGTH_MASK | LENGTH_ALIGN_MASK);
@@ -270,17 +293,21 @@ module regmap_tb;
     write_reg_and_update('h424, {$random} & STRIDE_MASK);
 
     check_all_registers("Transfer setup 2");
+    
 
     /* Start transfer */
     write_reg_and_update('h400, 'h01);
     write_reg_and_update('h408, 'h01);
-
+    set_reset_reg_value('h428, 32'h00000000);
+    set_reset_reg_value('h448, 24'h000000);
+    set_reset_reg_value('h44c, 32'hxxxxxxxx);
+    set_reset_reg_value('h450, 2'bX);
     check_all_registers("Transfer submitted");
 
     @(posedge clk) request_ready <= 1'b1;
 
     /* Interrupt pending */
-    set_reset_reg_value('h84, 'h01);
+    set_reset_reg_value('h84, 'h00);
     set_reset_reg_value('h88, 'h01);
 
     /* Transfer ID */
@@ -288,57 +315,68 @@ module regmap_tb;
 
     /* Tansfer pending */
     set_reset_reg_value('h408, 'h00);
-
+    set_reset_reg_value('h428, 32'h80000000);
+    set_reset_reg_value('h448, 24'h000080);
+    set_reset_reg_value('h44c, 32'h00000080);
+    set_reset_reg_value('h450, 'h0);
     check_all_registers("Transfer accepted");
 
     @(posedge clk) response_eot <= 1'b1;
     @(posedge clk) response_eot <= 1'b0;
 
     /* Interrupt registers */
-    set_reset_reg_value('h84, 'h03);
-    set_reset_reg_value('h88, 'h03);
+    set_reset_reg_value('h84, 'h00);
+    set_reset_reg_value('h88, 'h01);
 
-    /* Transfer done */
-    set_reset_reg_value('h428, 'h01);
-
+    
     /* Active ID */
-    set_reset_reg_value('h42c, 'h01);
-
+    set_reset_reg_value('h42c, 'h00);
+    set_reset_reg_value('h44c, 'h0);
+    set_reset_reg_value('h450, 'h0);
+    set_reset_reg_value('h428, 'h0);
+    set_reset_reg_value('h448, 24'h000080);
+    set_reset_reg_value('h44c, 32'h00000080);
+ 
     check_all_registers("Transfer completed");
 
     /* Clear interrupts */
     write_reg('h84, 'h01);
-    set_reset_reg_value('h84, 'h02);
-    set_reset_reg_value('h88, 'h02);
-
+    set_reset_reg_value('h84, 'h00);
+    set_reset_reg_value('h88, 'h00);
     check_all_registers("Clear interrupts 1");
 
     write_reg('h84, 'h02);
     set_reset_reg_value('h84, 'h00);
     set_reset_reg_value('h88, 'h00);
-
     check_all_registers("Clear interrupts 2");
-
+    
     /* Check that reset works for all registers */
     do_trigger_reset();
     initialize_expected_reg_mem();
+    write_reg_and_update('h40c, 'h00);
+    set_reset_reg_value('h44c, 32'h00000080);
     check_all_registers("Reset 1");
     invert_all_registers();
     do_trigger_reset();
+    set_reset_reg_value('h44c, 32'h00000080);
+    write_reg_and_update('h40c, 'h00);
     check_all_registers("Reset 2");
   end
 
   axi_dmac_regmap #(
     .ID(0),
+    .DISABLE_DEBUG_REGISTERS(0),
     .BYTES_PER_BEAT_WIDTH_DEST(BYTES_PER_BEAT),
     .BYTES_PER_BEAT_WIDTH_SRC(BYTES_PER_BEAT),
     .DMA_AXI_ADDR_WIDTH(DMA_AXI_ADDR_WIDTH),
     .DMA_LENGTH_WIDTH(DMA_LENGTH_WIDTH),
-    .DMA_LENGTH_ALIGN(DMA_LENGTH_ALIGN),
+    .DMA_LENGTH_ALIGN(LENGTH_ALIGN),
+    .BYTES_PER_BURST_WIDTH(7),
     .DMA_CYCLIC(1),
     .HAS_DEST_ADDR(1),
     .HAS_SRC_ADDR(1),
-    .DMA_2D_TRANSFER(1)
+    .DMA_2D_TRANSFER(1),
+    .SYNC_TRANSFER_START(0)
   ) i_axi (
     .s_axi_aclk(s_axi_aclk),
     .s_axi_aresetn(s_axi_aresetn),
@@ -361,7 +399,10 @@ module regmap_tb;
     .s_axi_rready(s_axi_rready),
     .s_axi_rresp(s_axi_rresp),
     .s_axi_rdata(s_axi_rdata),
-
+    
+    .ctrl_enable(ctrl_enable),
+    .ctrl_pause(ctrl_pause),
+       
     .request_valid(request_valid),
     .request_ready(request_ready),
     .request_dest_address(request_dest_address),
@@ -371,8 +412,15 @@ module regmap_tb;
     .request_dest_stride(request_dest_stride),
     .request_src_stride(request_src_stride),
     .request_last(request_last),
+    .request_sync_transfer_start(request_sync_transfer_start),
+
+    .irq(irq),
 
     .response_eot(response_eot),
+    .response_measured_burst_length(response_measured_burst_length),
+    .response_partial(response_partial),
+    .response_valid(response_valid),
+    .response_ready(response_ready),
 
     .dbg_src_addr(VAL_DBG_SRC_ADDR),
     .dbg_dest_addr(VAL_DBG_DEST_ADDR),
